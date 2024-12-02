88a499fc8df77902a34fad453f7564d134bf3ee4
Asterius8 <oscar.vg@tecjerez.edu.mx>
2024-12-02 01:00:09 -0600

    Creacion de la ventana Eliminar Alumno

--- Aplicacion_de_Escritorio/Proyecto_FinalSQLSERVER/src/main/java/Controlador/AlumnosDAO.java
+++ Aplicacion_de_Escritorio/Proyecto_FinalSQLSERVER/src/main/java/Controlador/AlumnosDAO.java
@@ -7,13 +7,12 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class AlumnosDAO {
-    
+
     //METODOS ---------------------------------------------------------------------------------------------------------------------
     //------------------------------------- Altas ------------------------------------------
     public static boolean agregarAlumno(Alumnos a) {
-        
+
         boolean res = false;
 
         res = ConexionBD.agregarAlumno(a);
@@ -21,35 +20,34 @@ public class AlumnosDAO {
         return res;
 
     }
-    
-    //------------------------------------- Bajas ------------------------------------------
 
+    //------------------------------------- Bajas ------------------------------------------
     
-        public static boolean BuscarNumControlIgualDAO(String filtro1) {
-        
+    
+    public static boolean BuscarNumControlIgualDAO(String filtro1) {
+
         try {
-            
+
             //ArrayList<Usuario> listaUsuarios = new ArrayList<>();
-            
             String sql = "SELECT * FROM Alumnos WHERE num_control ='" + filtro1 + "'";
-            
+
             ResultSet rs = ConexionBD.BuscarNumControlIgual(sql);
-            
+
             int rowCount = 0;
-            
+
             while (rs.next()) {
-                
+
                 rowCount++;
-                
+
             }
-            
+
             return rowCount == 0;
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return false;
-        
+
     }
 }
